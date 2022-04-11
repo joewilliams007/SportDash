14c16131e43df5a9df6a7e722d4c940f27efdf6b
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-04-11 18:23:36 +0200

    up

---
+++ app/src/main/java/com/stardash/sportdash/TermsOfServiceActivity.java
@@ -0,0 +1,14 @@
+package com.stardash.sportdash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TermsOfServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms_of_service);
+    }
+}
